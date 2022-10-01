3cd258f707627c79a106275dd01f2601977b9c72
Ashimov Rufat <rufatashimov55@gmail.com>
2022-10-01 20:13:15 +0600

    feat: 29-09

--- 27-09/script.js
+++ 27-09/script.js
@@ -1,6 +1,11 @@
-alert(1);
-alert(2);
-alert(3);
+let zeroNumber = '0';
+let firstNumber = '1';
+let second_number = '2';
+let ThirdNumber = '3';
+alert (zeroNumber);
+alert (firstNumber);
+alert (second_number);
+alert (ThirdNumber);
 
 let name1 = 'Rufat';
 let name2 = 'Ashimov Rufat';
---
+++ 29-09/script.js
@@ -0,0 +1,12 @@
+let str1 = 'САЛАМ, ';
+let str2 = 'БИШКЕК!';
+str1 + str2;
+alert (str1 + str2);
+
+const name = 'Rufat';
+const token = `Привет, ${name}!`;
+alert (token);
+
+const age = 16;
+const tok = `Мне ${age} лет!`;
+alert (tok);
