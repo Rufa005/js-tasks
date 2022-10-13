d91857c81a57eb3a265a5755c8ca65e1d05821a4
Ashimov Rufat <rufatashimov55@gmail.com>
2022-10-13 18:53:15 +0600

    feat: 11-10

---
+++ 11-10/script.js
@@ -0,0 +1,18 @@
+var userSalaries = {
+    Ella: 450,
+    Sophia: 550,
+    Ellie: 350
+};
+console.log(userSalaries.Ella,userSalaries.Sophia);
+
+var x = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+let result = x.a + x.b + x.c;
+console.log(result);
+
+let number = true;
+let date = (number === true) ? 3 : 5;
+console.log(date);
