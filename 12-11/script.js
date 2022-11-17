43c3842d07fc80db300bb91e39142a18e506dde5
Ashimov Rufat <rufatashimov55@gmail.com>
2022-11-17 20:33:07 +0600

    feat: 12-11

---
+++ 12-11/script.js
@@ -0,0 +1,11 @@
+document.querySelector('#container1').addEventListener('click', () => {
+    document.querySelector('#container1').classList.toggle('red');
+});
+
+document.querySelector('#container2').addEventListener('click', () => {
+    document.querySelector('#container2').classList.toggle('blue');
+});
+
+document.querySelector('#container3').addEventListener('click', () => {
+    document.querySelector('#container3').classList.toggle('gray');
+});
